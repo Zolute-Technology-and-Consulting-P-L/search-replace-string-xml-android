3358e13c3c14595d1cbd185c4e3e768b4aae2a1d
Hussain Jawadwala <hussain@jawadwala.com>
2021-07-21 12:25:26 +0530

    bug fixed

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import csv
 replace_rule = 'file.csv'
+stringfile = 'strings.xml'
 
-def doreplace(str1,replc,filename):
-    reading_file = open(filename, "r")
-    new_file_content = ""
-    for line in reading_file:
-        stripped_line = line.strip()
-        new_line = stripped_line.replace(str1, replc)
-        new_file_content += new_line +"\n"
-    reading_file.close()
-    writing_file = open("replaced-"+filename, "w")
-    writing_file.write(new_file_content)
-    writing_file.close()
+
+    
 
 with open(replace_rule, mode ='r')as file:
-   
-  # reading the CSV file
-  csvFile = csv.reader(file)
- 
-  # displaying the contents of the CSV file
-  for lines in csvFile:
-        print(lines[0]+" replaced by "+lines[1])
-        doreplace(lines[0],lines[1],'strings.xml')
+    # reading the CSV file
+    csvFile = csv.reader(file)
+    reading_file = open(stringfile, "r")
+    content = reading_file.read()
+    new_file_content = content
+    # displaying the contents of the CSV file
+    for lines in csvFile:
+        new_file_content = new_file_content.replace(lines[0], lines[1])
+   # print(new_file_content)
+    reading_file.close()
+    writing_file = open("replaced-"+stringfile, "w")
+    writing_file.write(new_file_content)
+    writing_file.close()
 
 
